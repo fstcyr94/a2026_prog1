35e9d55cf7a9fabacc35477a8e01c7843509244d
fstcyr <francis.st.cyr@outlook.com>
2026-08-26 09:50:29 -0400

    Exercices cours 2

---
+++ Bonjour2.py
@@ -0,0 +1,11 @@
+# Auteur : Francis St-Cyr
+# Date : 26 août 2026
+# Description : Exercice du cours #2
+
+age = 31
+nom = "Francis"
+
+print(nom)
+print(age)
+
+print(nom, age)
---
+++ democours2.py
@@ -0,0 +1,26 @@
+# Auteur : Francis St-Cyr
+# Date : 26 août 2026
+# Description : démonstration du cours #2
+
+
+print("ligne1")
+print("ligne2")
+print("ligne3")
+
+age = 18
+print(age)
+
+x = 10
+y = 20
+print(x+y)
+
+nombre1 = 10
+nombre2 = 20
+
+total = (
+    nombre1 +
+    nombre2
+)
+
+print(total)
+
