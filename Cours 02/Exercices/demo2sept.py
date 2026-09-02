9f1b13b347fb28ac4156d9db652102dc9c92fa9a
fstcyr <francis.st.cyr@outlook.com>
2026-09-02 09:53:49 -0400

    Create demo2sept.py

---
+++ Cours 02/Exercices/demo2sept.py
@@ -0,0 +1,46 @@
+#Auteur: Francis
+#Date : 2 septembre 2026
+#Description : Exemple du cours 2
+
+# nom_du_professeur = input("Bonjour, quel est le nom de ton professeur?")
+# print(f"Ton professeur se nomme {nom_du_professeur}")
+
+# contenu_du_type = type(nom_du_professeur)
+# print(f"Le type de ma variable est : {contenu_du_type}")
+
+
+#age = float(input("Quel est ton age"))
+#age = age + 1
+#print(f"Ton age est: {age}")
+
+# prix = float(input("Quel est le prix de la tomate?"))
+# print(f"La tomate a le prix de: {prix} $")
+# prix = prix + 1
+# print(prix)
+
+# prenom = "Francis"
+# nom = "St-Cyr"
+# ville = "Drummondville"
+
+# print(f"Mon nom est {prenom} {nom} et j'habite à {ville}")
+# print(prenom, nom, ville, sep=",", end="]")
+
+# print("allo",end="")
+# print("les",end="")
+# print("amis",end="")
+
+
+# print("Janvier", "Février", "Mars", sep=" - ")
+
+# valeur = 1.234567
+# print(f"{valeur:.5f}") #va arrondir la valeur float à la fin
+
+animal1 = "rat"
+animal2 = "Raton Laveur"
+animal3 = "Renard"
+
+print("--------------")
+print(f"|{animal1:<12}|")
+print(f"|{animal2:^12}|")
+print(f"|{animal3:>12}|")
+print(f"--------------")
