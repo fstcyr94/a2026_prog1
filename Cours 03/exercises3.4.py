24d55d402032369d2e0c53dd948c153ee6778529
fstcyr <francis.st.cyr@outlook.com>
2026-09-09 10:52:14 -0400

    Create exercises3.4.py

---
+++ Cours 03/exercises3.4.py
@@ -0,0 +1,17 @@
+a = 5
+b = 3
+c = 10
+d = 7
+p = True
+q = False
+
+# a. a > b = True
+# b. b >= c = False
+# c. not p = False
+# d. a == b or c > d = True
+# e. not (a == b) and d < c 
+# f. (a > b) and (b > c or d < c)
+# g. not (a > b and b > c) or q
+# h. (a > b or q) and (not p or d < c)
+# i. not (a > b) or (b > c and not q) or (d == c)
+# j. not ((a > b and c > d) or (q and not p)) and (b < a or c == d)
